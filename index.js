c78b33c29a78d19200970fabc822c091d98c94f2
CarlosEvCode <programer.cm12@gmail.com>
2026-03-12 15:31:27 -0500

    mejorando la estructura y manejo, agregando los metodos put y delete para productos

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const express = require('express') //Framework
-const mysql = require('mysql2') // Acceso BD
-const bodyParser = require('body-parser') //Interactuar con JSON
+const express = require('express'); //Framework
+const mysql = require('mysql2'); // Acceso BD
+const bodyParser = require('body-parser'); //Interactuar con JSON
 
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
 
 //Configuracion de acceso - .env
 const db = mysql.createConnection({
-    host:       'localhost',
-    user:       'root',
-    password:   '',
-    database:   'storeperu'
-})
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'storeperu',
+});
 
 //Aperturar la conexion
-db.connect((err) =>{
-    if(err) throw err;
-    console.log("Conectado a la DB de storeperu")
-})
+db.connect((err) => {
+	if (err) throw err;
+	console.log('Conectado a la DB de storeperu');
+});
 
 //Iniciar el servidor
-const PORT = 3000
+const PORT = 3000;
 
 ///* SERVICIOS WEB */
 //VERBO = ACCION = INTENCION
@@ -32,122 +32,124 @@ const PORT = 3000
 
 //req (requiere, requerimiento)
 //res (response,respuesta)
-app.post('/productos',(req,res)=>{
-    //Y los datos que queremos guardar? - DESERIALIZACION
-    const {id_marca,descripcion,precio,stock,garantia} = req.body
-    //? = comodin, evita ataques por SQLinjection
-    const sql = "INSERT INTO productos (id_marca,descripcion,precio,stock,garantia) VALUES (?,?,?,?,?)"
+app.post('/productos', (req, res) => {
+	//Y los datos que queremos guardar? - DESERIALIZACION
+	const {id_marca, descripcion, precio, stock, garantia} = req.body;
+	//? = comodin, evita ataques por SQLinjection
+	const sql =
+		'INSERT INTO productos (id_marca,descripcion,precio,stock,garantia) VALUES (?,?,?,?,?)';
 
-    db.query(sql,[id_marca,descripcion,precio,stock,garantia],(err,results) =>{
-        if(err) return res.status(500).send({
-            success:false,
-            message: 'No se concretó el registro'
-        })
+	db.query(
+		sql,
+		[id_marca, descripcion, precio, stock, garantia],
+		(err, results) => {
+			if (err)
+				return res.status(500).send({
+					success: false,
+					message: 'No se concretó el registro',
+				});
 
-        //Qué hacemos cuando logramos registrar?
-        res.send({
-            success: true,
-            message: 'Nuevo producto registrada',
-            id: results.insertId
-        })
-    })
-})
-app.get('/productos',(req,res)=>{
-    const sql = "SELECT * FROM productos LIMIT 10"
-    db.query(sql,(err,results)=>{
-        if(err) return res.status(500).send({message: 'Error acceso a datos'})
-        res.json(results)
-    })
-    /* res.send({'proceso': 'GET'}) */
-})
-app.post('/marcas',(req,res)=>{
-    //Y los datos que queremos guardar? - DESERIALIZACION
-    const {nombreM} = req.body
-    //? = comodin, evita ataques por SQLinjection
-    const sql = "INSERT INTO productos (nombreM) VALUES (?)"
+			//Qué hacemos cuando logramos registrar?
+			res.send({
+				success: true,
+				message: 'Nuevo producto registrada',
+				id: results.insertId,
+			});
+		}
+	);
+});
+app.get('/productos', (req, res) => {
+	const sql = 'SELECT * FROM productos LIMIT 10';
+	db.query(sql, (err, results) => {
+		if (err) return res.status(500).send({message: 'Error acceso a datos'});
+		res.json(results);
+	});
+	/* res.send({'proceso': 'GET'}) */
+});
+// Actualizar producto por ID
+app.put('/productos/:id', (req, res) => {
+	const {id} = req.params;
+	const {id_marca, descripcion, precio, stock, garantia} = req.body;
+	const sql = `UPDATE productos SET id_marca=?, descripcion=?, precio=?, stock=?, garantia=? WHERE id=?`;
+	db.query(
+		sql,
+		[id_marca, descripcion, precio, stock, garantia, id],
+		(err, results) => {
+			if (err) {
+				return res.status(500).send({
+					success: false,
+					message: 'No se concretó la actualización',
+					data: err,
+				});
+			}
+			return res.send({
+				success: true,
+				message: 'Producto actualizado',
+			});
+		}
+	);
+});
 
-    db.query(sql,[nombreM],(err,results) =>{
-        if(err) return res.status(500).send({
-            success:false,
-            message: 'No se concretó el registro'
-        })
+// Eliminar producto por ID
+app.delete('/productos/:id', (req, res) => {
+	const {id} = req.params;
+	const sql = 'DELETE FROM productos WHERE id = ?';
+	db.query(sql, [id], (err, results) => {
+		if (err) {
+			return res.status(500).send({
+				success: false,
+				message: 'No se puede eliminar el producto',
+			});
+		}
+		if (results.affectedRows == 0) {
+			return res.status(404).send({
+				success: false,
+				message: 'No existe el producto',
+			});
+		}
+		return res.send({
+			success: true,
+			message: 'Producto eliminado correctamente',
+		});
+	});
+});
 
-        //Qué hacemos cuando logramos registrar?
-        res.send({
-            success: true,
-            message: 'Nuevo producto registrada',
-            id: results.insertId
-        })
-    })
-})
-app.get('/marcas',(req,res)=>{
-    const sql = "SELECT * FROM marcas"
+app.post('/marcas', (req, res) => {
+	//Y los datos que queremos guardar? - DESERIALIZACION
+	const {nombreM} = req.body;
+	//? = comodin, evita ataques por SQLinjection
+	const sql = 'INSERT INTO marcas (nombreM) VALUES (?)';
 
-    db.query(sql,(err,results)=>{
-        if(err){
-            return res.status(500).send({
-                success:false,
-                message:"Error al obtener marcas"
-            })
-        }
+	db.query(sql, [nombreM], (err, results) => {
+		if (err)
+			return res.status(500).send({
+				success: false,
+				message: 'No se concretó el registro',
+			});
 
-        res.json(results)
-    })
-})
-//Se enviara el ID por la URL (endpoint)
-//Se enviara los datos por JSON
-/* app.put('/productos/:id',(req,res)=>{
-    const {id} = req.params
-    const {tipo,nombre,color,pesokg} =req.body
-    //Podemos escribir multilinea utilizando`
-    //COMODINES van en un indice
-    const sql = `UPDATE mascotas SET tipo=?,nombre=?,color=?,pesokg=? WHERE id =?`
-    db.query(sql, [tipo, nombre, color, pesokg, id],(err,results)=>{
-        if(err){
-            res.status(500).send({
-                seccess: false,
-                message: "No se concretó la actualizacion",
-                data: err
-            })
-        }
+		//Qué hacemos cuando logramos registrar?
+		res.send({
+			success: true,
+			message: 'Nuevo producto registrada',
+			id: results.insertId,
+		});
+	});
+});
+app.get('/marcas', (req, res) => {
+	const sql = 'SELECT * FROM marcas';
 
-        return res.send({
-            seccess: true,
-            message: "Registro actualizado"
-        })
-    })
-})
+	db.query(sql, (err, results) => {
+		if (err) {
+			return res.status(500).send({
+				success: false,
+				message: 'Error al obtener marcas',
+			});
+		}
 
-//En un WS para eliminar un registro, pasamos la PK como parte de la ruta
-//Ejemplo: DELETE - miwebservice.com/clientes/1
-app.delete('/mascotas/:id',(req,res)=>{
-    //De dónde obtenemos el ID a borrar? => ENDPOINT (URL)
-    const {id} = req.params
-    const sql = "DELETE FROM mascotas WHERE id = ?"
-    db.query(sql,[id], (err,results)=>{
-        //Clausula de GUARDA / retorno temprano
-        if(err){
-            res.status(500).send({
-                seccess: false,
-                message: 'No se puede eliminar el registro'
-            })
-        }
-        
-        if(results.affectedRows==0){
-            return res.status(404).send({
-            success: false,
-            message: 'No existe la mascota'
-            })
-        }
+		res.json(results);
+	});
+});
 
-        return res.send({
-            success: true,
-            message: 'Eliminado correctamente'
-        })
-        
-    })
-}) */
-
-app.listen(PORT, ()=>{
-    console.log("Servidor iniciado correctamente en http://localhost:3000");
-})
+app.listen(PORT, () => {
+	console.log('Servidor iniciado correctamente en http://localhost:3000');
+});
